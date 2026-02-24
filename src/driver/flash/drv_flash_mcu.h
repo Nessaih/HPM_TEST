3e6ac70f76413a31bd2603efdc6d44c57cf4dc95
Nessaih <fuwei_vic@163.com>
2026-02-24 17:02:30 +0800

    修复驱动参数名字错误

--- src/driver/flash/drv_flash_mcu.h
+++ src/driver/flash/drv_flash_mcu.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 int32_t drv_flash_mcu_init(void);
-int32_t drv_flash_mcu_erase(uint32_t addr, uint32_t pages);
+int32_t drv_flash_mcu_erase(uint32_t addr, uint32_t size);
 int32_t drv_flash_mcu_read(uint32_t addr, uint8_t *data, uint32_t len);
 int32_t drv_flash_mcu_write(uint32_t addr, const uint8_t *data, uint32_t len);
 
